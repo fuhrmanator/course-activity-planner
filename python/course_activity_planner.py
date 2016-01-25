8f2c2e3001f66cc68c8bf60168e5c77e4ab27c30
Justin Duplessis <drfoliberg@gmail.com>
2016-01-25 15:03:05 -0500

    Assert both ics and mbz are in request

--- python/course_activity_planner.py
+++ python/course_activity_planner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import uuid
+import json
 
 from flask import Flask, session, jsonify, request
 
@@ -8,9 +9,13 @@ app = Flask(__name__)
 
 @app.route('/api/planning', methods=['POST'])
 def post_planning():
-    req = request.get_json()
+    req = json.loads(request.form['data'])
     if not req or 'ics_url' not in req:
-        return jsonify({'message': 'Bad request'}), 400
+        return _bad_request()
+
+    f = request.files['file']
+    if not f:
+        return _bad_request()
 
     session['transaction_id'] = _generate_transaction_uuid()
     return jsonify({})
@@ -18,6 +23,10 @@ def post_planning():
 
 def _generate_transaction_uuid():
     return str(uuid.uuid4())
+
+
+def _bad_request(msg=None):
+    return jsonify({'message': 'Bad request.'}), 400
 
 
 def setup(env):
--- python/tests/app_test.py
+++ python/tests/app_test.py
@@ -1,6 +1,9 @@
+import io
 import json
 import unittest
 import course_activity_planner
+
+from werkzeug.datastructures import MultiDict
 
 
 class AppTest(unittest.TestCase):
@@ -19,15 +22,31 @@ class AppTest(unittest.TestCase):
             36, len(course_activity_planner._generate_transaction_uuid()))
 
     def test_cookie_is_set_after_posting_planning(self):
-        res = self.app.post('/api/planning', data=json.dumps({'ics_url': ''}),
-                            content_type='application/json')
+        res = self.app.post(
+            '/api/planning',
+            data=dict(file=(io.BytesIO(b'this is a test'), 'test.mbz'),
+                      data=json.dumps({'ics_url': ''})))
 
         self.assertEqual(200, res._status_code)
         assert res.headers['Set-Cookie'] is not None
 
     def test_cookie_is_not_set_after_bad_request(self):
+        # no ICS or MBZ
         res = self.app.post('/api/planning')
+        self.assertEqual(400, res._status_code)
+        assert 'Set-Cookie' not in res.headers
 
+        # No ICS url
+        res = self.app.post(
+            '/api/planning',
+            data=dict(file=(io.BytesIO(b'this is a test'), 'test.mbz')))
+        self.assertEqual(400, res._status_code)
+        assert 'Set-Cookie' not in res.headers
+
+        # No MBZ
+        res = self.app.post(
+            '/api/planning',
+            data=dict(data=json.dumps({'ics_url': ''})))
         self.assertEqual(400, res._status_code)
         assert 'Set-Cookie' not in res.headers
 
