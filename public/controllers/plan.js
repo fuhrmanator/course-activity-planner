4e67bbe1277b3e6cdb5a2b78310f39ab75ed439d
Justin Duplessis <drfoliberg@gmail.com>
2016-03-11 13:51:24 -0500

    Add exams to frontend

--- public/controllers/plan.js
+++ public/controllers/plan.js
@@ -2,10 +2,11 @@ var controllers = angular.module('app.controllers.PlanController', ['ngFileUploa
 
 controllers.controller('PlanController', function($scope, $http, $location, $routeParams) {
     $scope.uuid = $routeParams.uuid;
-    $scope.key_to_name = {'P': 'Practicas', 'S': 'Seminars', 'Q':'Quizzes', 'H': 'Homeworks', 'L':'Lessons', 'C': 'Choices', 'F': 'Feedbacks'};
+    $scope.key_to_name = {'P': 'Practicas', 'S': 'Seminars', 'Q':'Quizzes', 'H': 'Homeworks', 'L':'Lessons', 'C': 'Choices', 'F': 'Feedbacks', 'E': 'Exams'};
     $scope.meetingsKeys = ['S', 'P'];
     $scope.activitiesKeys = ['Q', 'H', 'L', 'C', 'F'];
-    $scope.previewKeys = $scope.meetingsKeys.concat($scope.activitiesKeys);
+    $scope.previewKeys = $scope.meetingsKeys.concat($scope.activitiesKeys)
+    $scope.previewKeys.push('E');
     $scope.keys = $scope.meetingsKeys.concat($scope.activitiesKeys);
 
     $scope.shownMeetingsKeys = $scope.meetingsKeys.slice(0);
