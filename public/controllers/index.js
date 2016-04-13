e99eab76eab50edceb60fe2241b4e38543183625
Justin Duplessis <drfoliberg@gmail.com>
2016-04-13 12:58:07 -0400

    Display name of planning of it's creation date

--- public/controllers/index.js
+++ public/controllers/index.js
@@ -13,6 +13,11 @@ controllers.controller('IndexController', function($scope, $http, $location, Upl
       }
     };
 
+    $scope.get_epoch = function (date_str) {
+      console.log(Date.parse(date_str));
+      return Date.parse(date_str);
+    };
+
     $scope.refresh = function() {
       $http.get('/api/planning/')
           .success(function(data) {
